937103c0c2c014603a3c6037d2af25f124c2f674
Michelle <scottmm374@gmail.com>
2022-12-04 14:09:41 -0500

    finished part 1

---
+++ Advent/2022/Day_4:_Camp_Cleanup/main.py
@@ -0,0 +1,60 @@
+
+
+def main():
+  inclusive_ranges = 0
+  
+  with open('sample.txt', 'r') as f:
+      for line in f.readlines():
+        check = check_ranges(line.strip())
+        print("results", check)
+        if check == True:
+          inclusive_ranges += 1
+      
+
+  return inclusive_ranges   
+
+def check_ranges(line):
+    ranges = [] 
+    new_line = line.replace("-",",")  
+    ranges.append(new_line.split(","))
+
+    # if int(ranges[0][0]) or int(ranges[0][1]) < int(ranges[0][2]):
+        # print("False", ranges)
+    #     return False
+    # elif int(ranges[0][0]) or int(ranges[0][1]) > int(ranges[0][3]):
+    #     print("False", ranges)
+    #     return False 
+    # else:  
+    #     print("True", ranges)
+    #     return True
+
+    if int(ranges[0][0]) <= int(ranges[0][2]) and int(ranges[0][1]) >= int(ranges[0][3]):
+        print("True", ranges)
+        return True
+    if int(ranges[0][0]) >= int(ranges[0][2]) and int(ranges[0][1]) <= int(ranges[0][3]):
+        print("True", ranges)
+        return True 
+    else:  
+      print("False", ranges)
+      return False
+
+
+
+    # PART 1
+    # if int(ranges[0][0]) <= int(ranges[0][2]) and int(ranges[0][1]) >= int(ranges[0][3]):
+    #     return True
+    # elif int(ranges[0][0]) >= int(ranges[0][2]) and int(ranges[0][1]) <= int(ranges[0][3]):
+    #     return True 
+    # else:  
+    #   return False
+
+
+    
+
+          
+        
+
+
+if __name__ == '__main__':
+    result = main()
+    print(result)
