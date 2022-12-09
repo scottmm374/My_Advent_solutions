883bd2e2eff8cbdd84146415045cda997b9acf34
Michelle <scottmm374@gmail.com>
2022-12-08 20:12:53 -0500

    still having issues with 1 extra visible, and one less blocked in test, Think it has to do with reference and value? OR indexes on edges

--- Advent/2022/Day8TreetopTreeHouse/main.py
+++ Advent/2022/Day8TreetopTreeHouse/main.py
@@ -14,56 +14,100 @@ forest = [[3,0,3,7,3], [2,5,5,1,2], [6,5,3,3,2], [3,3,5,4,9], [3,5,3,9,0]]
 # get number of trees on edge
 count_outside = ((len(forest) * 2) + (len(forest[0]) - 2) * 2)
 
-visible = []
-not_visible = []
+visible = 0
+not_visible = 0
 
 
 
 
 for tree_row in range(len(forest)):
     
-    for tree in range(len(forest[tree_row])):
+    for tree_col in range(len(forest[tree_row])):
         
+        tree = forest[tree_row][tree_col]
+       
+        if tree_row >= 1 and tree_col >= 1 and tree_row < len(forest) - 1 and tree_col < len(forest[tree_row]) - 1:
+           
 
-        if tree_row >= 1 and tree >= 1 and tree_row < len(forest) - 1 and tree < len(forest[tree_row]) - 1:
-
-            up = forest[tree_row - 1][tree]
-            down = forest[tree_row + 1][tree]
-            right = forest[tree_row][tree + 1]
-            left = forest[tree_row][tree - 1]
+            x = 1
+            up = forest[tree_row - x][tree_col]
+            print(up)
+            down = forest[tree_row + x][tree_col]
+            right = forest[tree_row][tree_col + x]
+            left = forest[tree_row][tree_col - x]
         
             blocked = 0
-            print(f'<-------Current coodinates {(tree_row, tree)}------->')
-            if forest[tree_row][tree] < up:
+            print(f'<-------Current coodinates {(tree_row, tree_col)}------->')
+            print(up)
+            # ! UP
+            if tree <= up:
                 blocked += 1
                 print("No going up")
+            
             else:
-                print("keep traveling UP", (tree_row, tree))
-
-        
-            if forest[tree_row][tree] < down:
+                while x != 0:
+                    print(f'Tree:{tree} Up:{up}')
+                    x = x - 1
+                    if tree < up:
+                        blocked +=1
+                        break
+            x = 1 
+            print(up)  
+                
+            # ! DOWN
+            if tree <= down:
                  blocked += 1
                  print("No going down")
-            else:
-                print("keep traveling DOWN", (tree_row, tree))
 
-
-            if forest[tree_row][tree] < right:
-                    blocked += 1
-                    print("No going right")
             else:
-                print("keep traveling RIGHT", (tree_row, tree))
+                while x < len(forest):
+                    print(f'Tree:{tree} Down:{down}')
+                    x = x + 1 
+                    if tree < down:
+                        blocked +=1
+                        break
+            x = 1   
+            print(up)   
+
                 
-            if forest[tree_row][tree] < left:
+            
+            # ! RiGHT
+            if tree <= right:
                     blocked += 1
                     print("No going right")
             else:
-                print("keep traveling LEFT", (tree_row, tree))
+                while x < len(forest[tree_col]):
+                    print(f'Tree:{tree} Right:{right}')
+                    x = x + 1 
+                  
+                    if tree < right:
+                        blocked +=1
+                        break
+            x = 1
+            print(up)   
 
 
+            #  ! LEFT
+            if tree <= left:
+                    blocked += 1
+                    print("No going left")
+            else:
+                while x != 0:
+                    print(f'Tree:{tree} Left:{left}')
+                    x = x - 1
+                    if tree < left:
+                        blocked +=1
+                        break
+            x = 1
+            print(up)   
 
+            print(blocked)
             if blocked == 4:
-                print(f"{(tree_row, tree)} is blocked height {forest[tree_row][tree]}")
+                not_visible +=1
+                print(f"{(tree_row, tree_col)} is blocked height {tree}")
+            else:
+                visible += 1
+                print(f"{(tree_row, tree_col)} is VISIBLE height {tree}")
         
 
-print(forest)
+print(f'{visible} Visible and {not_visible} NOT Visible')
